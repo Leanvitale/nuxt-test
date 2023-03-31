5e5b1f9b28bc303671b4e112380a9325c7eef36c
Leanvitale <vitale.lean@gmail.com>
2023-03-31 09:43:30 +0000

    Deploying to gh-pages from @ Leanvitale/nuxt-test@3acc3d63d40547a855b6e19dfa02f32a80eacb29 🚀

---
+++ _nuxt/about.1e94d12a.js
@@ -0,0 +1,1 @@
+import{a as e,b as t,o,e as c}from"./entry.1e0fc29b.js";const s={},n=c("h2",null,"About with Nuxt",-1),a=[n];function r(_,l){return o(),t("div",null,a)}const d=e(s,[["render",r]]);export{d as default};
