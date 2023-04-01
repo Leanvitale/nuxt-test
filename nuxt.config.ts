1d5cf5c5eac344c2ee0ac6c4b838c8b3f99e0b4e
Leandro Vitale <vitale.lean@gmail.com>
2023-04-01 12:04:08 +0200

    Add Sass, Prettier, i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    modules: ["@nuxtjs/tailwindcss"],
+    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
     srcDir: "src",
-    css: ["~/assets/css/main.css"],
+    css: ["~/assets/css/main.css", "~/assets/css/main.scss"],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
+        },
+    },
+    i18n: {
+        lazy: true,
+        langDir: "locales",
+        strategy: "prefix_and_default",
+        locales: [
+            {
+                code: "en",
+                name: "English",
+                file: "en.json",
+            },
+            {
+                code: "es",
+                name: "Spanish",
+                file: "es.json",
+            },
+        ],
+        defaultLocale: "en",
+        vueI18n: {
+            fallbackLocale: "en",
         },
     },
 });
